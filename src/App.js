327c7859100f5417de02f9d3b312c33cb0ca5c9e
Tatsuki Nagaoka <nagaoka@mbp.local>
2021-02-25 19:05:00 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
-  const profiles = [{name: 'Taro', age: 10}, {name: 'Hanako', age: 5}, {name: 'NoName'}];
+  const profiles = [
+    {name: 'Taro', age: 10},
+    {name: 'Hanako', age: 5},
+    {name: 'NoName', age: 3},
+  ];
   return (
     <div>
       {profiles.map((profile, index) => {
@@ -19,8 +24,9 @@ const User = (props) => {
   );
 };
 
-User.defaultProps = {
-  age: 1,
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired,
 };
 
 export default App;
